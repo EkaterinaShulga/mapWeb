a4f5ffb00ee1a22f27e90046a56a8ff98611936d
EkaterinaShulga <shulga23@gmail.com>
2022-06-20 18:10:53 +0300

    первый коммит

---
+++ src/main/java/pro/sky/java/course2/mapweb/MapWebApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.mapweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapWebApplication.class, args);
+    }
+
+}
